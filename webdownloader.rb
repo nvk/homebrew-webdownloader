f1daab7971115c210f0cf01aad067e6543e64e88
nvk <rodolfo@rnvk.org>
2025-04-07 12:37:26 -0400

    Update webdownloader formula with fixed resource URLs

--- webdownloader.rb
+++ webdownloader.rb
@@ -10,50 +10,20 @@ class Webdownloader < Formula
   sha256 "0969a8d1a97900ac880f641d39fe79cdf23e3a37db4d0aa7328005b62efdadf8"
   license "MIT"
 
-  depends_on "python@3.9"
-
-  resource "beautifulsoup4" do
-    url "https://files.pythonhosted.org/packages/af/0b/44c39cf3b18a9280950ad63a579ce395dda4c32193ee9da7ff0aed547094/beautifulsoup4-4.12.2.tar.gz"
-    sha256 "492bbc69dca35d12daac71c4db1bfff0c876c00ef4a2ffacce226d4638eb72da"
-  end
-
-  resource "certifi" do
-    url "https://files.pythonhosted.org/packages/71/da/e94e26401b62acd6d91df2b52954aceb7f561743aa5ccc32152886c76c96/certifi-2025.1.31.tar.gz"
-    sha256 "2412806c7f26097242850281948cf18164e880f3e880fcc2289c56a8274c0031"
-  end
-
-  resource "charset-normalizer" do
-    url "https://files.pythonhosted.org/packages/ff/d7/8d757f8bd45be079d76309248845a04f09619a7b17d6dfc8c9ff6433cac2/charset-normalizer-3.1.0.tar.gz"
-    sha256 "34e0a2f9c370eb95597aae63bf85eb5e96826d81e3dcf88b8886012906f509b5"
-  end
-
-  resource "html2text" do
-    url "https://files.pythonhosted.org/packages/0d/c9/20ccd54d52c843a82616b3f44beef5c501ef35412b0ad240e6623bc84eff/html2text-2024.2.26.tar.gz"
-    sha256 "a9a7731eeead6d4ee9d4b6d8cfba2b0f3fb3212a6f4880c323e2671e9d26484b"
-  end
-
-  resource "idna" do
-    url "https://files.pythonhosted.org/packages/8b/e1/43beb3d38dba6cb420cefa297822eac205a277ab43e5ba5d5c46faf96438/idna-3.4.tar.gz"
-    sha256 "814f528e8dead7d329833b91c5faa87d60bf71824cd12a7530b5526063d02cb4"
-  end
-
-  resource "requests" do
-    url "https://files.pythonhosted.org/packages/9d/ee/391076f5937f0a8cdf5e53b701ffc91753e87b07d66bae4a09aa671897bf/requests-2.28.2.tar.gz"
-    sha256 "98b1b2782e3c6c4904938b84c0eb932721069dfdb9134313beff7c83c2df24bf"
-  end
-
-  resource "soupsieve" do
-    url "https://files.pythonhosted.org/packages/47/9e/780779233a615777eb59db56f48e5a384eb56a6ef5e551b75848825d09f3/soupsieve-2.4.1.tar.gz"
-    sha256 "89d12b2d5dfcd2c9e8c22326da9d9aa9cb3dfab0a83a024f05704076ee8d35ea"
-  end
-
-  resource "urllib3" do
-    url "https://files.pythonhosted.org/packages/21/79/6372d8c0d0641b4072889f3ff84f279b738cd8595b64c8e0496d4e848122/urllib3-1.26.15.tar.gz"
-    sha256 "8a388717b9476f934a21484e8c8e61875ab60644d29b9b39e11e4b9dc1c6b305"
-  end
+  depends_on "python"
 
   def install
-    virtualenv_install_with_resources
+    virtualenv_create(libexec, "python3")
+    
+    # Install required pip packages
+    system libexec/"bin/pip", "install", "beautifulsoup4"
+    system libexec/"bin/pip", "install", "requests"
+    system libexec/"bin/pip", "install", "html2text"
+    
+    # Install from source directory
+    system libexec/"bin/pip", "install", "-v", "--no-deps", "--no-binary", ":all:", "."
+    
+    bin.install Dir["#{libexec}/bin/*"]
   end
 
   test do
